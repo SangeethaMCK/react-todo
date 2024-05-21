48b3b19a30dffa878e52f65c236d66fdd4f7d63b
SangeethaMCK <sangeeksd0509@gmail.com>
2024-05-21 14:40:23 +0530

    edit added

--- src/components/Inputtodo.jsx
+++ src/components/Inputtodo.jsx
@@ -1,6 +1,6 @@
 import React, { useState } from 'react';
 
-function InputTodo({ item, index, onDelete, onToggle }) {
+function InputTodo({ item, index, onEdit, onDelete, onToggle }) {
     const handleCheckboxChange = () => {
         onToggle(index);
     };
@@ -14,6 +14,7 @@ function InputTodo({ item, index, onDelete, onToggle }) {
             /></td>
             <td>{item.name}</td>
             <td>{item.date}</td>
+            <td><button className='button-edit' onClick={() => onEdit(index)}>EDIT</button></td>
             <td><button className='button-del' onClick={() => onDelete(index)}>DEL</button></td>
             </>
     );
--- src/components/Todo.jsx
+++ src/components/Todo.jsx
@@ -4,14 +4,35 @@ import InputTodo from './Inputtodo';
 export default function Todo() {
     const [todo, setTodo] = useState({ name: "", date: new Date().toISOString().split('T')[0], done: false });
     const [todos, setTodos] = useState([]);
+    const [editIndex, setEditIndex] = useState(null); 
 
     function handleSubmit(e) {
         e.preventDefault();
         if (todo.name.trim()) {
-            setTodos([...todos, todo]);
-            setTodo({ name: "", date: new Date().toISOString().split('T')[0], done: false });
+            if (editIndex === null) {
+                
+                setTodos([...todos, todo]);
+                setTodo({
+                    name: "",
+                    date: new Date().toISOString().split('T')[0],
+                    done: false
+                });
+            } else {
+                
+                const updatedTodos = todos.map((item, index) =>
+                    index === editIndex ? todo : item
+                );
+                setTodos(updatedTodos);
+                setTodo({
+                    name: "",
+                    date: new Date().toISOString().split('T')[0],
+                    done: false
+                });
+                setEditIndex(null);
+            }
         }
     }
+    
 
     const handleDelete = (index) => {
         const updatedTodos = todos.filter((todo, currentIndex) => currentIndex !== index);
@@ -23,6 +44,17 @@ export default function Todo() {
             currentIndex === index ? { ...todo, done: !todo.done } : todo
         );
         setTodos(updatedTodos);
+    };
+
+    const handleEdit=(index)=>{
+        const editTodo = todos[index];
+        setEditIndex(index); 
+
+        setTodo({
+            name:editTodo.name, 
+            date:editTodo.date, 
+            done:false});
+            
     };
 
     return (
@@ -42,7 +74,7 @@ export default function Todo() {
                     onChange={(e) => setTodo({ ...todo, date: e.target.value })}
                 />
                 <br />
-                <button className='button-add'type="submit">ADD</button>
+                <button className='button-add'type="submit" >ADD</button>
             </form>
             <table>
                 <thead>
@@ -58,6 +90,7 @@ export default function Todo() {
                             <InputTodo
                                 item={item}
                                 index={index}
+                                onEdit={handleEdit}
                                 onDelete={handleDelete}
                                 onToggle={handleToggle}
                             />
